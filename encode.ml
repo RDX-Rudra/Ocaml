1e548c94b9f4a82989d8e5a2cf5342e4cf90bf19
RDX-Rudra <dasrudra234@gmail.com>
2024-01-29 12:11:17 +0530

    ocaml

--- encode.ml
+++ encode.ml
@@ -14,3 +14,23 @@ let enc (c:char) (off:int) : char =
     
 let encstr (s: string) (off: int) : string = String.map (fun x -> enc x off) s;;
 
+let read_file filename = let ic = open_in filename in
+        let rec read_lines lines =
+          try
+            let line = input_line ic in
+            read_lines (line :: lines)
+          with End_of_file ->
+            close_in ic;
+            List.rev lines
+        in
+        read_lines []
+      ;;
+      
+      
+let encfile s off =let out = open_out (s^".enc") in
+  let f = read_file s in
+  List.iter (fun x -> Printf.fprintf out "%s\n" (encstr x off)) f;
+  close_out out
+    ;;
+
+encfile "one" 3;;
