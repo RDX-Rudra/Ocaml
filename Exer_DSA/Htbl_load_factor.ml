7568826112c78b436e6ee2550dabf6f1ede01722
Rdx-rudra <dasrudra234@gmail.com>
2024-02-23 15:06:45 +0000

    ocaml

--- Exer_DSA/Htbl_load_factor.ml
+++ Exer_DSA/Htbl_load_factor.ml
@@ -1,2 +1,10 @@
+let (/..) x y = float_of_int x /. float_of_int y
+
 let load_factor h =
-  
+  let stats = Hashtbl.stats h in 
+  stats.num_bindings /.. stats.num_buckets
+
+let epsilon = 0.1
+
+let close_to x y=
+abs_float (x-.y) <= epsilon
